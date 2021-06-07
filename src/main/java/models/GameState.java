cef6a58c48c46142e7d27961ca1d93246102de2b
pfengdev <pfengdev@gmail.com>
2021-06-06 23:31:56 -0700

    Displaying tic tac toe board

--- src/main/java/models/GameState.java
+++ src/main/java/models/GameState.java
@@ -8,12 +8,12 @@ public class GameState {
     private GridValue[][] grid;
     private String message;
 
-    private static final int SIZE = 3;
+    public static final int GRID_SIZE = 3;
 
     public GameState() {
-        grid = new GridValue[SIZE][SIZE];
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
+        grid = new GridValue[GRID_SIZE][GRID_SIZE];
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
                 grid[i][j] = GridValue.EMPTY;
             }
         }
--- src/main/java/renderer/Renderer.java
+++ src/main/java/renderer/Renderer.java
@@ -1,10 +1,30 @@
 package renderer;
 
+import enums.GridValue;
 import models.GameState;
 
 public class Renderer {
 
     public void render(GameState gameState) {
         System.out.println(gameState.getMessage());
+        printGrid(gameState);
+    }
+
+    private void printGrid(GameState gameState) {
+        GridValue[][] grid = gameState.getGrid();
+        for (int i = 0; i < GameState.GRID_SIZE; i++) {
+            System.out.print("|");
+            for (int j = 0; j < GameState.GRID_SIZE; j++) {
+                System.out.print(" ");
+                switch(grid[i][j]) {
+                    case EMPTY: System.out.print(" "); break;
+                    case PLAYER: System.out.print("O"); break;
+                    case COMPUTER: System.out.print("X"); break;
+                    default: break;
+                }
+                System.out.print(" |");
+            }
+            System.out.println();
+        }
     }
 }
